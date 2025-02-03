d45ab8b65a068002547ce34b8fb1e1382890a6e6
shivamVKalbande <shivamkalbande@gmail.com>
2025-02-03 14:08:46 +0530

    json parsing middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, {json, urlencoded} from 'express';
 import productsRoutes from './routes/products/index'
 
 const app = express();
 const port = 3000;
-
+app.use(urlencoded({ extended: false }));
+app.use(json()); //it is always above the routes
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
